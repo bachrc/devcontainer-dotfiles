d3525345013aa545859cc8be45f906663f43ecf5
bachrc <pacha@cyberendroit.net>
2026-01-29 14:00:43 +0100

    add starship config

--- prompt.sh
+++ prompt.sh
@@ -1,5 +1,7 @@
 mkdir -p ~/.local/bin
+mkdir -p ~/.config
 
 curl -sS -L https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz | tar xz -C ~/.local/bin
+cp ./config/starship.toml ~/.config/
 
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
